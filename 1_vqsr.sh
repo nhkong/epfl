73934e4bbb86169bb3b0a0e4daed0e118afba71f
nhkong <42317204+nhkong@users.noreply.github.com>
2018-08-13 15:17:42 +0200

    Create 1_vqsr.sh

---
+++ 1_vqsr.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+# Script filters a vcf according to per sample (not per batch) DP and GQ at a given site; 
+
+SnpSift=/home/nkong/snpEff/snpEff/SnpSift.jar
+
+DIR=/mnt/data1/HIV_SC/nkong/
+
+FILENAME=BNvs392.new.merged.vqsr.pass.se
+
+
+cat $DIR/$FILENAME.vcf | java -jar $SnpSift filter "( GEN[*].DP > 10 ) & ( GEN[*].GQ > 20 )" > $DIR/$FILENAME.vcf`
